4e179f9b7f1ef7cc550c865212177314aab6aefd
rcrworld <rcrchit@gmail.com>
2017-08-28 00:45:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,6 +30,83 @@ app.get('/test-db', function(req,res){
 
 
 
+
+var articles = {
+    'article-one' : {
+    title : 'Article one i kanth',
+    heading : 'Article one',
+    date : 'sep14,2017',
+    content : `
+    <p>
+    This is content of my first article.  This is content of my first article.  This is content of my first article.
+      This is content of my first article.  This is content of my first article.
+      </p>
+      <p>
+        This is content of my first article.  This is content of my first article.  This is content of my first article.
+          This is content of my first article.  This is content of my first article.
+          </p>
+          <p>
+            This is content of my first article.  This is content of my first article.  This is content of my first article.
+              This is content of my first article.  This is content of my first article.
+              </p>`
+              },
+              'article-two' : {
+                title : 'Article two i kanth',
+                heading : 'Article two',
+                date : 'sep11,2017',
+                content : 
+                `<p>
+                This is content of my second article.
+                </p>`
+              },
+              'article-three' : {
+                title : 'Article three i kanth',
+                heading : 'Article three',
+                date : 'sep9,2017',
+                content : 
+                `<p>
+                This is content of my third article.
+                </p>`
+              }
+};
+
+                function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmlTemplate= `
+<html>
+<head>
+    <title>
+        ${title}
+    </title>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class = "container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+            <h3>
+            ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+         ${content}
+        </div>
+    </div>
+    </body>
+    </html>
+    `;
+    return htmlTemplate;
+    }
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
