687960f9794e77b313d0938c8dbec8705d84d761
rcrworld <rcrchit@gmail.com>
2017-08-27 22:44:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var pool = new Pool(config);
-var Pool = require('pg').Pool;
+var pool = require('pg').Pool;
 
 var config = {
     
